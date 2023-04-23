4fce985a7803081e17f73f6b1126d18f9e42f90a
BennyBB97 <101838035+BennyBB97@users.noreply.github.com>
2023-04-24 00:58:13 +0200

    add client. Implement  start and move request

--- client/client.go
+++ client/client.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -15,6 +20,7 @@ func main() {
 			"pos": {
 				"x": 3,
 				"y": 4
+
 			} 
 		}
 	`)
@@ -31,4 +37,58 @@ func main() {
 	//Timestamp stop
 	timeEnd := time.Now()
 	timeFinal := timeBeginn.Sub(timeEnd)
+}
+
+type Pos struct {
+	X int
+	Y int
+	Z int
+}
+
+type vehiclePos struct {
+	Pos Pos
+	Id  uuid.UUID
+}
+
+func start(x int, y int) {
+	const serverUrl = "http://localhost:8080/v1/traffic/start"
+
+	var cor vehiclePos
+
+	requestBodyString := fmt.Sprintf(`
+	{
+		"pos": {
+			"x": %d,
+			"y": %d
+		} 
+	}
+	`, 15, 21)
+
+	requestBody := strings.NewReader(requestBodyString)
+
+	response, _ := http.Post(serverUrl, "application/json", requestBody)
+	content, _ := io.ReadAll(response.Body)
+	json.Unmarshal(content, &cor)
+	fmt.Println("Welcome to hs", cor)
+
+	move(cor.Pos.X, cor.Pos.Y, cor.Id.String())
+
+}
+
+func move(x int, y int, id string) {
+	const moveUrl = "http://localhost:8080/v1/traffic/move"
+	requestBodyString := fmt.Sprintf(`
+	{
+		"target": {
+			"x": %d,
+			"y": %d
+		},
+		"Id": "%s"
+	}
+	`, x, y, id)
+	fmt.Println(requestBodyString)
+	requestBody := strings.NewReader(requestBodyString)
+	response, _ := http.Post(moveUrl, "application/json", requestBody)
+	content, _ := io.ReadAll(response.Body)
+	fmt.Println("Welcome to hs", string(content))
 }
